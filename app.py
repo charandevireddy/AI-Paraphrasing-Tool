1b08d3561a90c4cd45d095e4d7e276cc3ed99e1f
charandevireddy <127002832+charandevireddy@users.noreply.github.com>
2025-03-05 12:16:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,12 +23,17 @@ os.environ["STREAMLIT_WATCHDOG"] = "false"
 nltk_data_path = os.path.expanduser("~/.nltk_data")
 os.makedirs(nltk_data_path, exist_ok=True)
 nltk.data.path.append(nltk_data_path)
+nltk.data.path.extend([
+    "/usr/share/nltk_data",
+    "/usr/local/share/nltk_data",
+    "/usr/lib/nltk_data",
+    "/usr/local/lib/nltk_data"
+])
 
 try:
     nltk.data.find('tokenizers/punkt')
 except LookupError:
     nltk.download('punkt', download_dir=nltk_data_path)
-    nltk.download('punkt_tab', download_dir=nltk_data_path)  # Ensure punkt_tab is also downloaded
     nltk.data.find('tokenizers/punkt')  # Ensure it is loaded
 
 # Load the paraphrasing model
