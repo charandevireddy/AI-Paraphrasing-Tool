292de3fc19a6cd7ec98d9727786cb32ce4193771
charandevireddy <127002832+charandevireddy@users.noreply.github.com>
2025-03-05 11:57:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,11 @@ os.environ["STREAMLIT_WATCHDOG"] = "false"
 nltk_data_path = os.path.expanduser("~/.nltk_data")
 os.makedirs(nltk_data_path, exist_ok=True)
 nltk.data.path.append(nltk_data_path)
-nltk.download('punkt', download_dir=nltk_data_path)
+
+try:
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt', download_dir=nltk_data_path)
 
 # Load the paraphrasing model
 @st.cache_resource
@@ -34,12 +38,7 @@ model, tokenizer, device = load_model()
 
 # Function to paraphrase text
 def paraphrase_text(text):
-    try:
-        sentences = sent_tokenize(text)
-    except LookupError:
-        nltk.download('punkt', download_dir=nltk_data_path)
-        sentences = sent_tokenize(text)
-    
+    sentences = sent_tokenize(text)
     paraphrased_sentences = []
 
     for sentence in sentences:
